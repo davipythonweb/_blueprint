b5871c15847b3d6abe598886d3d598d3452fd25b
davipythonweb <galaxys3tab32@gmail.com>
2026-05-23 15:46:25 -0300

    feat: add custom 404 error handler and improve main page rendering

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template_string
 from dotenv import load_dotenv
 import os
 
@@ -13,10 +13,28 @@ app = Flask(__name__)
 # Definir a chave secreta para a sessão (pode ser definida em um arquivo .env)
 app.secret_key = os.getenv('app.secret_key')
 
+# tratar erros 404 com uma página personalizada
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template_string('''
+    <div style="
+        padding:20px;
+        margin:20px;
+        background-color:#f2f2f2;
+    ">
+        <h1>ERRO 404! PAGE NOT FOUND!</h1>
+        <h2>Ops! A página que você está procurando não existe.</h2>
+        <p>Verifique se o URL está correto ou volte para a <a href="/">página principal</a>.</p>
+    </div>
+    ''')
+
+
 # rota publica para a pagina principal
 @app.route('/')
 def root():
-    return '<h1>Pagina Principal</h1>'
+    return render_template_string('''
+        <h1>Pagina Principal</h1>
+        ''')
 
 # Registrar o blueprint do usuário
 app.register_blueprint(user_bp, url_prefix='/user')
